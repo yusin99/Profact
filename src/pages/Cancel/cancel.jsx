11535a4c6267fd249ea5e08d5e51b5f8ea2c35ed
Yusin <sunkatabeckama@gmail.com>
2024-08-12 20:22:10 +0200

    Success payment and payment cancelled

---
+++ src/pages/Cancel/cancel.jsx
@@ -0,0 +1,11 @@
+const CancelPage = () => {
+    return (
+      <div>
+        <h1>Payment Cancelled</h1>
+        <p>Your payment was cancelled. Please try again.</p>
+        {/* Additional content or navigation */}
+      </div>
+    );
+  };
+  
+  export default CancelPage;
---
+++ src/pages/Success/success.jsx
@@ -0,0 +1,11 @@
+const SuccessPage = () => {
+  return (
+    <div>
+      <h1>Payment Successful</h1>
+      <p>Thank you for your purchase!</p>
+      {/* Additional content or navigation */}
+    </div>
+  );
+};
+
+export default SuccessPage;
