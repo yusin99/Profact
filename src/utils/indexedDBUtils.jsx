3ace826f1f18f2616de009ecfa1a957c62304c0e
Yusin <sunkatabeckama@gmail.com>
2024-08-25 09:34:04 +0200

    Adding clear comments for indexdb

--- src/utils/indexedDBUtils.jsx
+++ src/utils/indexedDBUtils.jsx
@@ -1,9 +1,14 @@
-// utils/indexedDBUtils.js
-
 const DB_NAME = 'TokenStore';
 const STORE_NAME = 'tokens';
 const TOKEN_KEY = 'accessToken';
 
+/**
+ * Initializes the IndexedDB database.
+ * This function opens a connection to the database and creates the necessary object store if it doesn't already exist.
+ * 
+ * @returns {Promise<IDBDatabase>} - A promise that resolves to the database instance.
+ * @throws {Error} - Throws an error if there is an issue with opening the IndexedDB connection.
+ */
 export const initDB = () => {
   return new Promise((resolve, reject) => {
     const request = indexedDB.open(DB_NAME, 1);
@@ -19,6 +24,14 @@ export const initDB = () => {
   });
 };
 
+/**
+ * Stores the authentication token in IndexedDB.
+ * This function saves the provided token in the database using a specific key.
+ * 
+ * @param {string} token - The authentication token to be stored.
+ * @returns {Promise<void>} - A promise that resolves when the token is successfully stored.
+ * @throws {Error} - Throws an error if there is an issue with storing the token.
+ */
 export const setToken = async (token) => {
   const db = await initDB();
   return new Promise((resolve, reject) => {
@@ -31,6 +44,13 @@ export const setToken = async (token) => {
   });
 };
 
+/**
+ * Retrieves the authentication token from IndexedDB.
+ * This function fetches the token associated with the specific key from the database.
+ * 
+ * @returns {Promise<string|null>} - A promise that resolves to the token value, or null if the token is not found.
+ * @throws {Error} - Throws an error if there is an issue with retrieving the token.
+ */
 export const getToken = async () => {
   const db = await initDB();
   return new Promise((resolve, reject) => {
